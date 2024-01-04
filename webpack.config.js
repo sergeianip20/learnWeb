379ceb98c0ea718f9978066949dcb8b7de357b4a
sergeianip20 <sergei_anip20@mail.ru>
2024-01-04 19:55:44 +0900

    update function webpackconfig/

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: {
-    app: './src/index.js',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-    hot: true,
-  },
- module: {
-   rules: [
-     {
-       test: /\.css$/,
-       use: ['style-loader', 'css-loader'],
-     },
-   ],
- },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Hot Module Replacement',
-    }),
-  ],
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  mode: 'development',
- optimization: {
-   usedExports: true,
- },
-};
+module.exports = ()=>{
+  return {
+    entry: {
+      app: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+      hot: true,
+    },
+   module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use: ['style-loader', 'css-loader'],
+       },
+     ],
+   },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Hot Module Replacement',
+      }),
+    ],
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    mode: 'development',
+   optimization: {
+     usedExports: true,
+   },
+  };
+} 
