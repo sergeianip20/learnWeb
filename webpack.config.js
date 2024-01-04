1b2535d9d4a96af26a840ef47b4026f98a579b86
sergeianip20 <sergei_anip20@mail.ru>
2024-01-04 13:49:38 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+ module: {
+   rules: [
+     {
+       test: /\.css$/,
+       use: ['style-loader', 'css-loader'],
+     },
+   ],
+ },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Hot Module Replacement',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
